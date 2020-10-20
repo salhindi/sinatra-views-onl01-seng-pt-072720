f67cff3384b48b448465f1c74ee40ef56c06b876
Sara A <calisaraalhindi@gmail.com>
2020-10-20 00:22:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,16 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
-	get '/' do
-		"<h1>Hello World</h1>"
-	end
 	
 	get '/' do
     erb :index
   end
   
   get '/info' do
-    "Testing the info page"
+    erb :info
   end
+
 end
